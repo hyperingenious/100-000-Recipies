50ed43099c26e0e5311fd26a201f4d775c9b9c78
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-01 06:38:19 +0530

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,39 +1,59 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
+    // LOADING....
+    recipeView.renderSpinner();
+
+    // 1. Fetching Data
+    await model.loadRecipe(id);
+
+    //Rendering Recipe
+    recipeView.render(model.state.recipe);
+
+    // Displaying Data
+  } catch (e) {
+    // alert(e);
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // 1. Get Search query
+    const query = searchView.getQuery();
+
+    if (!query) return;
+
+    // loading spinnner
+    resultsView.renderSpinner();
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const init = function () {
-  const controlRecipes = async function () {
-    try {
-      const id = window.location.hash.slice(1);
-
-      if (!id) return;
-
-      // LOADING....
-      recipeView.renderSpinner();
-
-      // 1. Fetching Data
-      await model.loadRecipe(id);
-
-      //Rendering Recipe
-      recipeView.render(model.state.recipe);
-
-      // Displaying Data
-    } catch (e) {
-      // alert(e);
-      recipeView.renderError();
-    }
-  };
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
 
 if (module.hot) {
   module.hot.accept();
 }
-
-model.loadSearchResults('pizza');
