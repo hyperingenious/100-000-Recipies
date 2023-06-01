e041fed31a8988e670a8f5c8621b45a69d89df13
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-01 06:38:30 +0530

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,65 +1,18 @@
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
+import View from './View.js';
 import { mark } from 'regenerator-runtime';
 
-class RecipeView {
-  #data;
-  #parentElement = document.querySelector('.recipe');
-  #message = 'Recipe does not found, Please try another one';
-
-  render(data) {
-    this.#data = data;
-    const markup = this.#genrateMarkup(this.#data);
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderSpinner() {
-    this.#clear();
-    const html = `<div class="spinner">
-          <svg>
-            <use href="${icons}#icon-loader"></use>
-          </svg>
-        </div> `;
-    this.#parentElement.insertAdjacentHTML('afterbegin', html);
-  }
-  #clear() {
-    this.#parentElement.innerHTML = '';
-  }
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _message = 'Recipe does not found, Please try another one';
+  _errorMessage = 'We could not find that recipe. Please try another one';
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
-  renderError(message = this.#message) {
-    const markup = `
-      <div class="error">
-              <div>
-                <svg>
-                  <use href="${icons}#icon-alert-triangle"></use>
-                </svg>
-              </div>
-              <p>${message}</p>
-            </div>
-          `;
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-  renderMessage(message) {
-    const markup = `
-      <div class="message">
-              <div>
-                <svg>
-                  <use href="${icons}#icon-smile"></use>
-                </svg>
-              </div>
-              <p>${message}</p>
-            </div>
-          `;
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
 
-  #genrateMarkup(data) {
+  _generateMarkup(data) {
     return `
     <figure class="recipe__fig">
             <img src="${data.image}" alt="Tomato" class="recipe__img" />
@@ -99,11 +52,7 @@ class RecipeView {
                 </button>
               </div>
             </div>
-
             <div class="recipe__user-generated">
-              <svg>
-                <use href="${icons}#icon-user"></use>
-              </svg>
             </div>
             <button class="btn--round">
               <svg class="">
@@ -115,7 +64,7 @@ class RecipeView {
           <div class="recipe__ingredients">
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
-            ${data.ingredients.map(this.#generateMarkupIngredients).join('')}
+            ${data.ingredients.map(this._generateMarkupIngredients).join('')}
             </ul>
           </div>
 
@@ -139,7 +88,7 @@ class RecipeView {
           </div>
     `;
   }
-  #generateMarkupIngredients(ing) {
+  _generateMarkupIngredients(ing) {
     return ` <li class="recipe__ingredient">
                 <svg class="recipe__icon">
                   <use href="${icons}#icon-check"></use>
