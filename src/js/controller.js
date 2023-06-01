c34f8f2db8bb865a62f53b9f9dbdf8b6c223c369
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-01 14:16:09 +0530

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // const recipeContainer = document.querySelector('.recipe');
 
@@ -42,7 +43,10 @@ const controlSearchResults = async function () {
 
     // 2. Load search results
     await model.loadSearchResults(query);
-    resultsView.render(model.state.search.results);
+
+    // resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage(2));
+    paginationView.render(model.state.search);
   } catch (err) {
     console.error(err);
   }
@@ -53,7 +57,3 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
 };
 init();
-
-if (module.hot) {
-  module.hot.accept();
-}
