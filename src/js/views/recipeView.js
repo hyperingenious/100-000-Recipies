822fa8d9219f97f60d9edabdc00cf91b57c2976a
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-02 18:16:18 +0530

    Update recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -31,26 +31,26 @@ class RecipeView extends View {
               <span class="recipe__info-text">minutes</span>
             </div>
             <div class="recipe__info">
-              <svg class="recipe__info-icon">
-                <use href="${icons}#icon-users"></use>
-              </svg>
-              <span class="recipe__info-data recipe__info-data--people">${
-                data.servings
-              }</span>
-              <span class="recipe__info-text">servings</span>
+                  <svg class="recipe__info-icon">
+                    <use href="${icons}#icon-users"></use>
+                  </svg>
+                  <span class="recipe__info-data recipe__info-data--people">${
+                    data.servings
+                  }</span>
+                  <span class="recipe__info-text">servings</span>
 
-              <div class="recipe__info-buttons">
-                <button class="btn--tiny btn--increase-servings">
-                  <svg>
-                    <use href="${icons}#icon-minus-circle"></use>
-                  </svg>
-                </button>
-                <button class="btn--tiny btn--increase-servings">
-                  <svg>
-                    <use href="${icons}#icon-plus-circle"></use>
-                  </svg>
-                </button>
-              </div>
+                  <div class="recipe__info-buttons">
+                    <button class="btn--tiny btn--decrease-servings">
+                      <svg>
+                        <use href="${icons}#icon-minus-circle"></use>
+                      </svg>
+                    </button>
+                    <button class="btn--tiny btn--increase-servings">
+                      <svg>
+                        <use href="${icons}#icon-plus-circle"></use>
+                      </svg>
+                    </button>
+                  </div>
             </div>
             <div class="recipe__user-generated">
             </div>
