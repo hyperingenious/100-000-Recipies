e5ec52b8b4bafffa1b4b14a2e261785c9675949e
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-06 19:43:42 +0530

    Update addRecipeView.js

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -2,8 +2,34 @@ import icons from 'url:../../img/icons.svg';
 import View from './View.js';
 
 class AddRecipeView extends View {
-  _parentElement = document.querySelector('.upload'); // FORM elemnt from html
+  _parentElement = document.querySelector('.upload'); // FORM element from HTML
+
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
+  _btnOpen = document.querySelector('.nav__btn--add-recipe');
+  _btnClose = document.querySelector('.btn--close-modal');
+
+  constructor() {
+    super();
+    this._addHandlerShowWindow();
+  }
+  toggleWindow() {
+    this._overlay.classList.toggle('hidden');
+    this._window.classList.toggle('hidden');
+  }
+  _addHandlerShowWindow() {
+    this._btnOpen.addEventListener('click', this.toggleWindow.bind(this));
+    this._overlay.addEventListener('click', this.toggleWindow.bind(this));
+  }
+  addHandlerUpload(handler) {
+    this._parentElement.addEventListener('submit', function (e) {
+      e.preventDefault();
+      const dataArr = [...new FormData(this)];
+      const data = Object.fromEntries(dataArr);
+      console.log(data);
+
+      handler(data);
+    });
+  }
 }
 export default new AddRecipeView();
