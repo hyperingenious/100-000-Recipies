8c302a6288062fd61fbe450d8208a42ef4fe2ef4
Saurav Meghwal <skbmasale941@gmail.com>
2023-06-06 19:43:50 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const PAGE_CAPACITY = 13;
+export const KEY = '757ac58c-9020-472f-97b9-a7f553500647';
